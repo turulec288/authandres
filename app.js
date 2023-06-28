36d6f136e2ff9e2a9eb35eef1c558c6a5be4184a
FRITZ IRONHACK <69217268+turulec288@users.noreply.github.com>
2023-06-28 11:45:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv/config");
+require("./db");
+const express = require("express");
+const { isAuthenticated } = require("./middleware/jwt.middleware"); // <== IMPORT
+const app = express();
+
+require("./config")(app);
+
+// 👇 Start handling routes here
+const allRoutes = require("./routes");
+app.use("/api", allRoutes);
+
+const projectRouter = require("./routes/project.routes");
+app.use("/api", isAuthenticated, projectRouter);      
+
+const taskRouter = require("./routes/task.routes");
+app.use("/api", isAuthenticated, taskRouter);    
+
+const authRouter = require("./routes/auth.routes");       //  <== IMPORT
+app.use("/auth", authRouter);                             //  <== ADD
+
+require("./error-handling")(app);
+
+module.exports = app;
